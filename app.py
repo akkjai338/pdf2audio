9794e2d3d75511ce492f2a691f12c0cdc884fb09
akkjai338 <akkjai338@gmail.com>
2026-02-22 13:40:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask, render_template, request, send_file
 from PyPDF2 import PdfReader
-from gtts import gTTS
 import os
 import uuid
+import asyncio
+import edge_tts
 
 app = Flask(__name__)
 
@@ -13,23 +13,27 @@ AUDIO_FOLDER = "audio"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
+async def generate_voice(text, output_file):
+    communicate = edge_tts.Communicate(
+        text,
+        voice="en-IN-PrabhatNeural"  # Male Indian voice (Hinglish friendly)
+    )
+    await communicate.save(output_file)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/convert", methods=["POST"])
 def convert():
-    language = request.form.get("language")
     text = request.form.get("text")
     file = request.files.get("pdf_file")
 
     extracted_text = ""
 
-    # If user entered text
     if text and text.strip() != "":
         extracted_text = text
 
-    # If user uploaded PDF
     elif file:
         unique_id_pdf = str(uuid.uuid4())
         pdf_path = os.path.join(UPLOAD_FOLDER, unique_id_pdf + ".pdf")
@@ -39,21 +43,18 @@ def convert():
         for page in reader.pages:
             if page.extract_text():
                 extracted_text += page.extract_text()
-
     else:
-        return "Please enter text or upload a PDF"
+        return "Please enter text or upload PDF"
 
     if extracted_text.strip() == "":
         return "No readable text found"
 
-    # Limit text length
     extracted_text = extracted_text[:5000]
 
     unique_id_audio = str(uuid.uuid4())
     audio_path = os.path.join(AUDIO_FOLDER, unique_id_audio + ".mp3")
 
-    tts = gTTS(text=extracted_text, lang=language)
-    tts.save(audio_path)
+    asyncio.run(generate_voice(extracted_text, audio_path))
 
     return send_file(audio_path, as_attachment=True)
 
